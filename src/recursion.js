d0a3264e17171643cb34fc1a3788996f301d92e8
Kim Luu <kimluuwdev@gmail.com>
2020-05-25 16:45:31 -0500

    complate until num4 part1

--- src/recursion.js
+++ src/recursion.js
@@ -5,28 +5,63 @@
 // 1. Calculate the factorial of a number. The factorial of a non-negative integer n,
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
+//5! = 5 x 4!
+//4! = 4 x 3! , 3! = 3 x 2!
 // factorial(5); // 120
 var factorial = function(n) {
+    if (n < 0) {
+        return null;
+     } else if (n>=0 && n<=1) {
+         return 1;
+     } else if (n>1) {
+         return n * factorial(n-1);}
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    let arr = array.slice(0);
+    if (arr.length === 0) {
+        return 0;
+    } else if (arr.length === 1) {
+         return arr[0];
+    } else {
+        return arr[0] += sum(arr.slice(1));
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+    let sum = 0;
+    for (let i=0; i<array.length; i++) {
+        let numOrArr = array[i];
+        if (!Array.isArray(numOrArr)) {       
+        sum += array[i];
+        } else {
+            sum += arraySum(numOrArr);
+        }
+    }
+    return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    if (n===0) {
+        return true;
+    } else if (n ===1 || n===-1) {
+        return false;
+    } else if (n>=2 || n<=-2) {
+        return isEven((n/2-Math.floor(n/2))*2);     
+    }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+    
+    
 };
 
 // 6. Get the integers within a range (x, y).
